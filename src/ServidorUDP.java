597559b844f51843eb56b19ccedf8e87fe3523f1
Dennis-Hidalgo <dennisf00@hotmail.com>
2024-03-17 18:57:18 +0100

    file sending

--- src/ServidorUDP.java
+++ src/ServidorUDP.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +7,8 @@ public class ServidorUDP {
     private static final int PORT = 12345;
     private static final int BUFFER_SIZE = 1024;
     private static List<InetAddress> direccionesConectadas = new ArrayList<>();
+
+    private static List<String> nombreClientes = new ArrayList<>();
     private static List<Integer> puertosConectados = new ArrayList<>();
     private static final String RUTA = System.getProperty("user.home") + "/Desktop/users/";
 
@@ -41,6 +42,7 @@ public class ServidorUDP {
         if (!isClientKnown) {
             direccionesConectadas.add(address);
             puertosConectados.add(port);
+            nombreClientes.add(nombreCliente);
             System.out.println("Nuevo cliente registrado: " + nombreCliente + " - " + address.toString() + ":" + port);
             // Informa al grupo de que el cliente ha entrado
             for (int i = 0; i < direccionesConectadas.size(); i++) {
@@ -54,9 +56,37 @@ public class ServidorUDP {
                 System.out.println("Carpeta creada para el cliente: " + nombreCliente);
             }
         } else {
-            // Enviar el mensaje a todos los clientes conectados
-            for (int i = 0; i < direccionesConectadas.size(); i++) {
-                enviarMensaje(socket, message, direccionesConectadas.get(i), puertosConectados.get(i));
+            // Si el mensaje es para enviar un archivo
+            if (message.startsWith("!file ->")) {
+                String filePath = message.substring("!file ->".length()).trim();
+                File fileToSend = new File(filePath);
+                if (fileToSend.exists() && fileToSend.isFile()) {
+                    // Copiar el archivo a la carpeta repository
+                    File repositoryFolder = new File(RUTA + "/repository");
+                    if (!repositoryFolder.exists()) {
+                        repositoryFolder.mkdirs();
+                    }
+                    File copiedFile = new File(repositoryFolder, fileToSend.getName());
+                    copyFile(fileToSend, copiedFile);
+                    // Enviar el archivo a cada carpeta de cliente conectado
+                    for (int i = 0; i < direccionesConectadas.size(); i++) {
+                        File clientFolder = new File(RUTA + nombreClientes.get(i));
+                        System.out.println("direcciones: #" + nombreClientes + "#");
+                        if (!clientFolder.exists()) {
+                            clientFolder.mkdirs();
+                        }
+                        File destinationFile = new File(clientFolder, copiedFile.getName());
+                        copyFile(copiedFile, destinationFile);
+                        enviarMensaje(socket, "has recibido el fichero '" + copiedFile.getName() + "'.", direccionesConectadas.get(i), puertosConectados.get(i));
+                    }
+                } else {
+                    enviarMensaje(socket, "El archivo especificado no existe o no es un archivo válido.", address, port);
+                }
+            } else {
+                // Enviar el mensaje a todos los clientes conectados
+                for (int i = 0; i < direccionesConectadas.size(); i++) {
+                    enviarMensaje(socket, message, direccionesConectadas.get(i), puertosConectados.get(i));
+                }
             }
         }
     }
@@ -66,5 +96,16 @@ public class ServidorUDP {
         byte[] sendData = message.getBytes();
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, port);
         socket.send(sendPacket);
+    }
+
+    // Copia un archivo a otro destino
+    private static void copyFile(File source, File destination) throws IOException {
+        try (InputStream in = new FileInputStream(source); OutputStream out = new FileOutputStream(destination)) {
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+        }
     }
 }
